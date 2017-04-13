1e723493d62a6b9e0669b19961b9392a931dac03
SwissWolfQueen <katidzoni@gmail.com>
2017-04-13 11:52:15 +0200

    final step browser&gulp

---
+++ gulpfile.js
@@ -0,0 +1,9 @@
+var gulp = require('gulp');
+var ghPages = require('gulp-gh-pages');
+
+var path = "./platforms/browser/www";
+
+gulp.task('deploy', function() {
+  return gulp.src(path+'/**/*')
+    .pipe(ghPages());
+});
